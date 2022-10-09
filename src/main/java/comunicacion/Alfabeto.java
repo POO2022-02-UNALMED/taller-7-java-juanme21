91fa4168a26c3f63ff46c1e9d4c05380cf5f2783
juanme21 <106550509+juanme21@users.noreply.github.com>
2022-10-08 22:16:00 -0500

    zUVHV99_lSh2nPIQKzcPZhO

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,31 @@
+package comunicacion;
+
+import comunicacion.Pictograma;
+
+public class Alfabeto extends Pictograma {
+
+
+	String[] letras;
+	String interpretacion;
+	
+	
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	@Override
+	String interpretacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/comunicacion/Libro.java
@@ -0,0 +1,29 @@
+package comunicacion;
+
+public class Libro extends Escrito {
+
+	private String co_autor;
+	private String editor;
+	private String edicon;
+	private String interpretacion; 
+	
+	
+	@Override
+	int palabrasTotales(int palabrasPagina) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	String interpretacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
